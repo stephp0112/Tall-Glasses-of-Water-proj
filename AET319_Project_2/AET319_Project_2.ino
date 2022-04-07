eb38ec5f9298e0c1670f79baa8050f723084c8b2
stephp0112 <stephperez@utexas.edu>
2022-04-07 14:58:12 -0500

    if button pressed, swivel servo

--- AET319_Project_2/AET319_Project_2.ino
+++ AET319_Project_2/AET319_Project_2.ino
@@ -1,9 +1,9 @@
 /*
- AET Tangled Enclosure
- Team Name: Tall Glasses of Water
- Team Members: Breianna, Amarachi, and Steph
- Description: button and servo program
- */
+  AET Tangled Enclosure
+  Team Name: Tall Glasses of Water
+  Team Members: Breianna, Amarachi, and Steph
+  Description: button and servo program
+*/
 
 //Servo code library
 #include<Servo.h>;
@@ -14,11 +14,17 @@ const int buttonPin = 7;
 
 void setup() {
   // put your setup code here, to run once:
-myServo.attach(9);
-pinMode(buttonPin, INPUT);
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //if button is pressed, swivel servo 180 degrees
+  if (digitalRead(buttonPin) == HIGH) {
+    myServo.write(180);
+  } else {
+    myServo.write(0);
+  }
 
 }
